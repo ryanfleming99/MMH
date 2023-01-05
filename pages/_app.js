cfd3a079df03dc6ee62f27e5997b9af5023fdd8c
Ryan Fleming <ryanfleming93@aol.com>
2023-01-05 19:02:00 +0000

    adding next.js setup

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../styles/globals.css";
+const MyApp = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+export default MyApp;
---
+++ scripts/getname.js
@@ -0,0 +1,4 @@
+const getName = () => {
+  const name = document.querySelector("name").innerHTML;
+  console.log(name);
+};
