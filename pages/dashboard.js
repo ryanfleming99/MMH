dd4eb0e9b71661ece8637f325ce763492df9c8ac
Wojciech <bojawojt@gmail.com>
2023-02-14 13:45:11 +0000

    stable pricing and checkout pages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from "next/router"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from "../lib/firebase/firebase"
 
 
-const [user] = useAuthState(auth)
-
-if (!user) {
-    const router = useRouter()
-    router.push("/")
-}
 
 const dashboard = () => {
+    const [user, loading, error] = useAuthState(auth)
+
+
+
     return (
         <div>dashboard</div>
     )
