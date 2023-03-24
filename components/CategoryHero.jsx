32d7008b1090c6cb335a254d97f3c9102a2c29a8
Ryan Fleming <ryanfleming93@aol.com>
2023-03-24 00:09:53 +0000

    exercise design updates

--- components/CategoryHero.jsx
+++ components/CategoryHero.jsx
@@ -8,10 +8,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 const CategoryHero = ({ title, content1, content2, content3, bg }) => {
   return (
-    <section style={{ background: `url(${content3})` }}>
+    <section
+      style={{ background: `url(${content3})` }}
+      className="lg:w-4/5 mx-auto"
+    >
       {/* Navbar End */}
       <div className="lg:w-6/12 md:w-3/4 sm:w-3/4 my-2">
-        <div className="h-[75vh] flex justify-center align-center flex-col margin m-auto">
+        <div className="py-48 flex justify-center align-center flex-col margin m-auto">
           <HeroHeading title={title} />
           <p className="text-white my-2 text-base leading-8">{content1}</p>
           <p className="text-white my-2 text-base leading-8">{content2}</p>
